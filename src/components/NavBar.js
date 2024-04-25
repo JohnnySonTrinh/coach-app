41e069490077acd6c3652894181fd491871e3da6
Johnny Trinh <johnny.trinh@hotmail.se>
2024-04-25 04:08:56 +0200

    Add function for active router style to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,15 +2,34 @@ import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.png";
 import styles from "../styles/NavBar.module.css";
+// import { NavLink } from "react-router-dom";
+
+import { NavLink } from "react-router-dom";
+
+function CustomNavLink({ children, to, ...props }) {
+  return (
+    <NavLink
+      to={to}
+      {...props}
+      className={({ isActive }) =>
+        isActive ? `${styles.NavLink} ${styles.Active}` : styles.NavLink
+      }
+    >
+      {children}
+    </NavLink>
+  );
+}
 
 const NavBar = () => {
   return (
     <Navbar expand="md" className={styles.NavBar} fixed="top">
       <Container fluid>
-        <Navbar.Brand href="#">
-          <img src={logo} alt="logo" height={45} />
-          <span className="d-none d-md-inline">Star Review</span>
-        </Navbar.Brand>
+        <NavLink to="/">
+          <Navbar.Brand href="#">
+            <img src={logo} alt="logo" height={45} />
+            <span className="d-none d-md-inline">Star Review</span>
+          </Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
@@ -18,15 +37,18 @@ const NavBar = () => {
             style={{ maxHeight: "200px" }}
             navbarScroll
           >
-            <Nav.Link>
-              <i className="fa fa-house"></i>Home
-            </Nav.Link>
-            <Nav.Link>
-              <i className="fa fa-sign-in"></i>Sign in
-            </Nav.Link>
-            <Nav.Link>
-              <i className="fa fa-envelope"></i>Newsletter
-            </Nav.Link>
+            <CustomNavLink to="/">
+              {" "}
+              <i className="fa fa-house"></i> Home{" "}
+            </CustomNavLink>
+            <CustomNavLink to="/signin">
+              {" "}
+              <i className="fa fa-sign-in"></i> Sign in{" "}
+            </CustomNavLink>
+            <CustomNavLink to="/newsletter">
+              {" "}
+              <i className="fa fa-envelope"></i> Newsletter{" "}
+            </CustomNavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
