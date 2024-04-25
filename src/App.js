54268ccf27877ee5cd82227977133dbc70747e79
Johnny Trinh <johnny.trinh@hotmail.se>
2024-04-25 06:00:07 +0200

    Create Sign up form and add route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from "./components/NavBar";
 import Container from "react-bootstrap/Container";
 import { Route, Routes } from "react-router-dom";
 import "./api/axiosDefaults";
+import SignUpForm from "./pages/auth/SignUpForm";
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<h1>Home page</h1>} />
           <Route path="/signin" element={<h1>Sign In</h1>} />
-          <Route path="/signup" element={<h1>Sign up</h1>} />
+          <Route path="/signup" element={<SignUpForm />} />
           <Route path="/newsletter" element={<h1>Newsletter</h1>} />
           <Route path="*" element={<h1>Page not found!</h1>} />
         </Routes>
