525975c1d393680af1b4d8c1dfdc05f422802f23
Johnny Trinh <johnny.trinh@hotmail.se>
2024-04-27 04:40:15 +0000

    down grade react

--- src/App.js
+++ src/App.js
@@ -1,48 +1,24 @@
-import styles from "./App.module.css";
-import NavBar from "./components/NavBar";
-import Container from "react-bootstrap/Container";
-import { Route, Routes } from "react-router-dom";
-import "./api/axiosDefaults";
-import SignUpForm from "./pages/auth/SignUpForm";
-import SignInForm from "./pages/auth/SignInForm";
-import { createContext, useState, useEffect } from "react";
-import axios from "axios";
-
-export const CurrentUserContext = createContext();
-export const SetCurrentUserContext = createContext();
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [currentUser, setCurrentUser] = useState(null);
-
-  const handleMount = async () => {
-    try {
-      const { data } = await axios.get("/dj-rest-auth/user/");
-      setCurrentUser(data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  useEffect(() => {
-    handleMount();
-  }, []);
-
   return (
-    <CurrentUserContext.Provider value={currentUser}>
-      <SetCurrentUserContext.Provider value={setCurrentUser}>
-        <div className={styles.App}>
-          <NavBar />
-          <Container className={styles.Main}>
-            <Routes>
-              <Route path="/" element={<h1>Home page</h1>} />
-              <Route path="/signin" element={<SignInForm />} />
-              <Route path="/signup" element={<SignUpForm />} />
-              <Route path="/newsletter" element={<h1>Newsletter</h1>} />
-              <Route path="*" element={<h1>Page not found!</h1>} />
-            </Routes>
-          </Container>
-        </div>
-      </SetCurrentUserContext.Provider>
-    </CurrentUserContext.Provider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- src/api/axiosDefaults.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-axios.defaults.baseURL =
-  "https://coach-platform-api-b2a0c10b1c34.herokuapp.com/";
-axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
-axios.defaults.withCredentials = true;
